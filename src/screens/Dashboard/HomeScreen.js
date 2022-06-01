aec46a81ce9f22dccad3ccd52ce9caa6a12874c0
monique.dingding <monique.dingding@lamudi.com>
2022-06-02 03:51:44 +0800

    feat: open links when workout starts

--- src/screens/Dashboard/HomeScreen.js
+++ src/screens/Dashboard/HomeScreen.js
@@ -1,45 +1,78 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { StyleSheet, View, ScrollView, Modal, Pressable } from 'react-native'
+import React, { useState, useRef, useEffect, useCallback } from 'react'
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  Modal,
+  Pressable,
+  Linking
+} from 'react-native'
 import { Button, Card, Icon, Text } from '@ui-kitten/components'
 import AuthLayout from '../../layout/AuthLayout'
 import BottomNavigationSection from '../../components/BottomNavigationSection'
 import ExploreSection from '../../components/ExploreSection'
 import HabitTrackerSection from '../../components/HabitTrackerSection'
-import store from '../../store'
 import withNavigation from '../../hoc/withNavigation'
 
 const UpperSection = () => {
   const [isWorkoutStarted, setWorkoutStarted] = useState(false)
+  const [buttonValue, setButtonValue] = useState('WORKOUT_STARTED')
+  const url = 'https://www.instagram.com/p/Cd0BDIphjc6/?igshid=YmMyMTA2M2Y='
 
-  const onPress = value => {
-    setWorkoutStarted(!value)
-  }
+  const onPressOpenLink = useCallback(
+    async value => {
+      // Checking if the link is supported for links with custom URL scheme.
+      const supported = await Linking.canOpenURL(url)
+      await setWorkoutStarted(!value)
 
-  const TimerIcon = props => (
-    <Icon
-      {...props}
-      name='clock-outline'
-      />
+      if (!value) {
+        await setButtonValue('WORKOUT_IN_PROGRESS')
+        if (supported) {
+          // Opening the link with some app, if the URL scheme is "http" the web link should be opened
+          // by some browser in the mobile
+          await Linking.openURL(url)
+        } else {
+          Alert.alert(`Don't know how to open this URL: ${url}`)
+        }
+      } else {
+        await setButtonValue('WORKOUT_FINISHED')
+      }
+    },
+    [url]
   )
+
+  const TimerIcon = props => <Icon {...props} name='clock-outline' />
 
   return (
     <>
       <View style={styles.subtext}>
-        <Text category='s2' status='primary'>TODAY</Text>
+        <Text category='s2' status='primary'>
+          TODAY
+        </Text>
       </View>
       <View style={styles.row}>
         <Text category='h6'>
-          Conditions are never perfect. Now is the time to
-          create the life you want.
+          Conditions are never perfect. Now is the time to create the life you
+          want.
         </Text>
       </View>
-      <Button 
-        onPress={() => onPress(isWorkoutStarted)}
-        style={styles.installButton}
-        status={isWorkoutStarted ? 'warning' : 'primary'}
-        >
-          { isWorkoutStarted ? 'FINISH WORKOUT' : 'START WORKOUT' }
-      </Button>
+      {!isWorkoutStarted && buttonValue === 'WORKOUT_FINISHED' ? (
+        <Button style={styles.installButton} disabled>
+          YOU DID GREAT TODAY!
+        </Button>
+      ) : 
+      <Button
+      onPress={() => onPressOpenLink(isWorkoutStarted)}
+      style={styles.installButton}
+      status={isWorkoutStarted ? 'warning' : 'primary'}
+      value={url}
+      disabled={!isWorkoutStarted && buttonValue === 'WORKOUT_FINISHED'}
+    >
+      {isWorkoutStarted ? 'FINISH WORKOUT' : 'START WORKOUT'}
+    </Button>
+      }
+
+      
     </>
   )
 }
@@ -90,19 +123,19 @@ const styles = StyleSheet.create({
   },
   centeredView: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 22
   },
   modalView: {
     margin: 20,
-    backgroundColor: "white",
+    backgroundColor: 'white',
     borderRadius: 20,
     padding: 35,
     width: '80%',
     height: '40%',
-    alignItems: "center",
-    shadowColor: "#000",
+    alignItems: 'center',
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
       height: 2
